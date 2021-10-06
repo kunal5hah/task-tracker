0c1fd10fbc67391f13d7bad5b03370bc62b4f687
Kunal Shah <roughblot@gmail.com>
2021-10-06 23:48:44 +0530

    Added more components

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+// Created a header template
+
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @btn-click="this.$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Add Task'" 
+        :color="showAddTask ? 'red' : 'green'"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean,
+    },
+    components: {
+        Button,
+    },
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task 
+        @toggle-reminder="this.$emit('toggle-reminder', task.id)"
+        @delete-task="this.$emit('delete-task', task.id)" v-bind:task="task"/>
+    </div>
+</template>
+
+<script>
+
+import Task from './Task'
+
+export default{
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        Task,
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
